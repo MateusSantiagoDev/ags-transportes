36bb1ec251ad36c88f78f6e46ee0dcea9480e614
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-08 19:22:11 -0300

    chore: header and start update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.css";
 import Logo from "../../assets/Logo.png";
+import fundoEdt from "../../assets/fundoEdt.png";
 import { Link, useNavigate } from "react-router-dom";
 import { CgCloseO } from "react-icons/cg";
 import Modal from "react-modal";
@@ -22,7 +23,35 @@ export function Header() {
   } */
 
   return (
-    <nav className="nav_header">
+      <div className="div_geral-header">
+        
+        <div className="div_vazia0">
+        <div className="div_vazia1"></div>
+        <div className="div_vazia2"></div>
+
+        </div>
+        <div className="div_logo">
+          <img src={Logo} alt="logo" />
+          <div className="div_link">
+          <Link className="link link_inicio" to={"/"}>
+          <h4 className="h4_link">Inicio</h4>
+        </Link>
+        <Link className="link" to={"/sobre-nos"}>
+          <h4 className="h4_link">Sobre nós</h4>
+        </Link>
+        <Link className="link" to={"/servicos"}><h4 className="h4_link">Serviços</h4></Link>
+          
+        
+          <a className="button_contato link" href="/" target="_blanc"><h4 className="h4_link">Contate-nos</h4></a>
+          </div>
+          <div className="div_vazia3"></div>
+        </div>
+      </div>
+
+   /*  <div>
+      <div className="div_vazia"></div>
+      <div className="div_header">
+      <div>     
       <img src={Logo} alt="logo da empresa" />
       <div className="div_header">
         <Link className="link link_inicio" to={"/"}>
@@ -36,6 +65,9 @@ export function Header() {
         
           <a className="button_contato link" href="/" target="_blanc"><h4>Contate-nos</h4></a>
       </div>
+      </div>
+      </div>
+     
       {/* <div className="div_modal">
         <Modal
           className="modal"
@@ -89,7 +121,7 @@ export function Header() {
             </div>
           </form>
         </Modal>
-      </div> */}
-    </nav>
+      </div> 
+    </div> */
   );
 }
--- src/Pages/inicio/inicio.jsx
+++ src/Pages/inicio/inicio.jsx
@@ -1,11 +1,16 @@
 import "./inicio.css";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Logo from "../../assets/Logo.png";
 import { RiWhatsappFill, RiInstagramFill } from "react-icons/ri";
 import { GrFacebook } from "react-icons/gr";
 import { TfiEmail } from "react-icons/tfi";
+import { motion } from "framer-motion";
 import video1 from "../../assets/movies/video1.mp4";
+import inicio1 from "../../assets/inicio1.jpg";
+import inicio2 from "../../assets/inicio2.jpg";
+import inicio3 from "../../assets/inicio3.jpg";
+import inicio4 from "../../assets/inicio4.jpg";
 
 export function Inicio() {
   const [data, setData] = useState([]);
@@ -24,26 +29,98 @@ export function Inicio() {
     };
     ////// metodo da api //////
   }
+
+  const images = [inicio1, inicio2, inicio3, inicio4, inicio1, inicio2]
+
+  const slider = useRef();
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(slider.current?.scrollWidth - slider.current?.offsetWidth);
+  }, []);
+
+  return (
+    <div className="div_inicio">
+      <div className="div_slider">
+       <motion.div
+          ref={slider}
+          className="slider"
+          whileTap={{ cursor: "grabbing" }}
+        > 
+          <motion.div
+            className="inner"
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            initial={{ x: 100 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            {images.map((el) => (
+              <motion.div className="el" key={el}>
+                <img src={el} alt="image-slider" />
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div> 
+        </div> 
+
+        <div>
+          <div className="cotacao">
+            <h2>faça uma cotação online e sem custo</h2>
+          </div>
+          <div className="texto">
+            <h1>transporte sob medida para a sua nescessidade!</h1>
+          </div>
+        </div>
+    </div>
+  )
+
+  /* const images = [inicio1, inicio2, inicio3, inicio4];
+
+  const slider = useRef();
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(slider.current?.scrollWidth - slider.current?.offsetWidth);
+  }, []);
+
   return (
     <div className="div_div">
-      
-      
-        <div className="div_inicio">
-        <div className="div_h2">
-          <h2>O melhor do brasil em um único lugar</h2>
-        </div>
-        <div className="div_button-cotacao">
-        <button className="button_cotacao" onClick={() => {
-          navegate("/form")
-        }}>      
-          <h4>Faça uma cotação</h4>
-          <h2>On-line & Sem Custo</h2>
-        
-      </button>
-      </div>
-      </div>
+      <div className="div_inicio">
+        <motion.div
+          ref={slider}
+          className="slider"
+          whileTap={{ cursor: "grabbing" }}
+        >
+          <motion.div
+            className="inner"
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            initial={{ x: 100 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            {images.map((el) => (
+              <motion.div className="el" key={el}>
+                <img src={el} alt="image-slider" />
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
+      </div> */
+      {/*   <div className="div_button-cotacao">
+          <button
+            className="button_cotacao"
+            onClick={() => {
+              navegate("/form");
+            }}
+          >
+            <h4>Faça uma cotação</h4>
+            <h2>On-line & Sem Custo</h2>
+          </button>
+        </div> */}
 
-      <div className="div_video">
+      {/* <div className="div_video">
         <video autoPlay muted loop className="video">
           <source className="video_mp4" src={video1} type="video/mp4" />
         </video>
@@ -78,6 +155,7 @@ export function Inicio() {
           </a>
         </li>
       </ul>
-    </div>
-  );
+    </div> 
+  );*/
+}
 }
